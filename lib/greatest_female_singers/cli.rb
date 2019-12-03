f8d1182b7c32376d569c262039bb5fbd4a2a3571
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-03 15:54:57 +0000

    call method,cli.rb

--- lib/greatest_female_singers/cli.rb
+++ lib/greatest_female_singers/cli.rb
@@ -2,9 +2,37 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-class GreatestFemaleSingers :: CLI
+class CLI
   
-  def run 
-    puts "Welcome to Top Greatest Female Singers!"
+  def call 
+    
+    input = ""
+    while input != "exit"
+    
+    puts "Welcome to Top Ten Greatest Female Singers!"
+    puts "To guess the number one greatest female singer, enter 'guess singer'."
+    puts "To see the list of top ten greatest female singers, enter 'list singers'."
+    puts "To quit, type 'exit'."
+    puts "So, what will it be?"
+    
+    input gets.strip 
+    
+    case input 
+    
+    when 'guess singer'
+      guess_singer
+    
+    when 'list singers'
+      list_singers
+      
+    # when 'exit'
+      # exit 
+    
+  else 
+    puts "Please make a choice."
+  
   end 
+  
+
+  
 end 
