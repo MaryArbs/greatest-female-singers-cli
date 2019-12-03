39314940329a29336157e0a5dbd984149e20518b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-03 20:29:25 +0000

    update gemspec

--- lib/greatest_female_singers/scraper.rb
+++ lib/greatest_female_singers/scraper.rb
@@ -1,5 +1,11 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
+  
+  def self.scrape_page
+    Nokogiri::HTML(open("https://www.imdb.com/list/ls064792627/"))
+  end
+  binding.pry 
 end 
