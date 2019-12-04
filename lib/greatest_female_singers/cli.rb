39f070aa242771197d6ff9965159cc392ae9845e
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-04 19:12:49 +0000

    update cli.rb

--- lib/greatest_female_singers/cli.rb
+++ lib/greatest_female_singers/cli.rb
@@ -46,40 +46,54 @@ class CLI
      else 
       puts "Please make a choice."
   
+    end 
   end 
+end 
   
   def diana_ross
-  end
+    if input == 'Diana Ross'
+      return "Close, but no cigar!"
+    end
+  end 
   
   def tina_turner
+    if input == 'Tina Turner'
+      return "Close, but no cigar!"
+    end 
   end 
   
   def aretha_franklin
+    if input == 'Aretha Franklin'
+      return "WOOHOO, you got it!"
+    end 
   end 
   
   def whitney_houston
+    if input == 'Whitney Houston'
+      return "Although I agree with you that she is the best singer ever, this answer is not correct!"
+      binding.pry
+    end 
   end 
-end 
   
-  def list_singers
-    puts "Here are the greatest female singers!"
-    Singer.all.each.with_index(1) do |singer, idex|
-      puts "#{idex}. #{singer.name}"
+end 
+  # def list_singers
+  #   puts "Here are the greatest female singers!"
+  #   Singer.all.each.with_index(1) do |singer, idex|
+  #     puts "#{idex}. #{singer.name}"
     
-    puts "Enter a number 1-#{Singer.all.size} to find out more about a singer."
-    input = gets.chomp.to_i
+  #   puts "Enter a number 1-#{Singer.all.size} to find out more about a singer."
+  #   input = gets.chomp.to_i
     
-    if valid_input(input)
-      puts "Cool! Let's learn more!"
-    else 
-      puts "Try again. You need to enter a number 1-10."
-      input = gets.chomp.to_i
-    end
+  #   if valid_input(input)
+  #     puts "Cool! Let's learn more!"
+  #   else 
+  #     puts "Try again. You need to enter a number 1-10."
+  #     input = gets.chomp.to_i
+  #   end
     
-  def valid_input(input)
-    input.between?(1,Singer.all.size)
-  end 
+  # def valid_input(input)
+  #   input.between?(1,Singer.all.size)
+  # end 
   
 
   
-end 
--- lib/greatest_female_singers/scraper.rb
+++ lib/greatest_female_singers/scraper.rb
@@ -7,5 +7,5 @@ class Scraper
   def self.scrape_page
     Nokogiri::HTML(open("https://www.imdb.com/list/ls064792627/"))
   end
-  binding.pry 
+
 end 
