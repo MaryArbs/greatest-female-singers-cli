a6ca5615bc0687ce5cfd2dd272278901658867cc
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-05 15:50:41 +0000

    update scraper.rb

--- lib/greatest_female_singers/scraper.rb
+++ lib/greatest_female_singers/scraper.rb
@@ -5,13 +5,20 @@ require '..lib/greatest_female_singers.rb'
 
 class GreatestFemaleSingers::Scraper
   
-  def self.scrape_singers
+  def self.scrape_index_page(index_url)
    site = "https://www.imdb.com/list/ls064792627/"
    doc = Nokogiri::HTML(open(site))
+   singers = []
    list_of_singers = doc.css("h3.lister-item-header")
    list_of_singers.each.with_index(1) {|section, index| 
     puts  "#{index}." + section.css('a').text}
-    binding.pry
-    
+    singers << list_of_singer
+    url = section.css ('a')[0]['href']
+  end 
+  
+  def self.scrape_profile_page (profile_url)
   end 
 end 
+
+ #main > div > div.lister.list.detail.sub-list > div.lister-list > div:nth-child(1) > div.lister-item-content > h3 > a
+ #main > div > div.lister.list.detail.sub-list > div.lister-list > div:nth-child(2) > div.lister-item-content > h3 > a
--- lib/greatest_female_singers/singer.rb
+++ lib/greatest_female_singers/singer.rb
@@ -4,16 +4,16 @@ class GreatestFemaleSingers::Singer
   
   @@all = []
   
-  def initialize(name, url)
-    @name = name
-    @url = url 
-    self.save 
+  def initialize(singer_hash)
+    singer_hash.each {|attribute, value| self.send(("#{attribute}="), value)} 
+    #iterating through the student_hash attributes, assinging each attribute method(setter method) a value- (self=instance of singer)
+    
+    @@all << self   #shoveling each instance of singer into the @@all class variable 
   end 
+    
   
   def self.all
-     @@all
+     @@all      # allows access to the @@all variable 
   end
 
-  def save
-    @@all << self
-  end
+  
